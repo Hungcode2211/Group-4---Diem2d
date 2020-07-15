ec627a9b602b28975a65ccd8dfd925c79181f1bb
Hung212 <kazamakristsugaya@gmail.com>
2020-07-15 18:06:05 +0700

    My First Commit

---
+++ Diem2D.cs
@@ -0,0 +1,87 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DiemKhongGian
+{
+  
+    public class Diem2D
+    {
+        
+        float hoanhDo;
+        float tungDo;
+
+        
+        public Diem2D()
+        {
+            hoanhDo = tungDo = 0f;
+        }
+
+       
+        public Diem2D(float x)
+        {
+            this.hoanhDo = x;
+            tungDo = 0;
+        }
+        
+        // hàm khởi tạo truyền vào hoành độ và tung độ
+        public Diem2D(float x, float y)
+        {
+            this.hoanhDo = x;
+            this.tungDo = y;
+        }
+
+        // hai thành phần trả về hoành độ và tung độ
+        public float HoanhDo { get => hoanhDo; }
+        public float TungDo { get => tungDo; }
+
+        // phương thức cộng.
+       
+        public Diem2D Cong(Diem2D b)
+        {
+            return new Diem2D(b.hoanhDo + this.hoanhDo, b.tungDo + this.tungDo);
+        }
+
+        // phương thức trừ.
+       
+        public Diem2D Tru(Diem2D b)
+        {
+            return new Diem2D(this.hoanhDo - b.hoanhDo, this.tungDo - b.tungDo);
+        }
+
+        // tính khoảng cách của điểm hiện tại và điểm b bằng công thức: sqrt((a - c)^2 + (b - d)^2) với X(a, b), Y(c, d)
+        public double TinhKhoangCach(Diem2D b)
+        {
+            double ketqua = (this.hoanhDo - b.hoanhDo) * (this.hoanhDo - b.hoanhDo) + (this.tungDo - b.tungDo) * (this.tungDo - b.tungDo);
+            return Math.Sqrt(ketqua);
+        }
+
+        // tính điểm đối xứng qua tâm O(0, 0) bằng công thức: với Y(0 - a, 0 - b), với Y là điểm đối xứng của X(a, b).
+        public Diem2D DoiXung()
+        {
+            return new Diem2D(0 - this.hoanhDo, 0 - this.tungDo);
+        }
+
+        // nhập điểm cho Diem2D
+        public void NhapDiem2D()
+        {
+            Console.Write("\nDiem M(x, y), nhap x = ");
+            while(!float.TryParse(Console.ReadLine(), out this.hoanhDo))
+                Console.Write("Nhập lại x = ");
+
+            Console.Write("\nDiem M({0}, y), nhap y = ", this.hoanhDo);
+            while (!float.TryParse(Console.ReadLine(), out this.tungDo))
+                Console.Write("Nhap lại y = ");
+        }
+
+        //kết quả dạng string, có dạng (x, y).
+        public string ConvertToString()
+        {
+            return "(" + hoanhDo + ", " + tungDo + ")";
+        }
+    }
+   
+}
+
---
+++ Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DiemKhongGian
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Diem2D diem2d = new Diem2D();
+            diem2d.NhapDiem2D();
+            
+            
+            Console.WriteLine("Diem 2D vua nhap: " + diem2d.ConvertToString());
+
+            Diem2D diem2d1 = new Diem2D(3, -2);
+            Console.WriteLine("Diem 2D thu 2: " + diem2d1.ConvertToString());
+
+            Console.WriteLine("\nTong hai diem: " + diem2d.Cong(diem2d1).ConvertToString());
+            Console.WriteLine("\nHieu hai diem: " + diem2d.Tru(diem2d1).ConvertToString());
+            Console.WriteLine("\nKhoang cach diem {0} va {1} la: {2}" , diem2d.ConvertToString(), diem2d1.ConvertToString(), diem2d.TinhKhoangCach(diem2d1));
+            Console.WriteLine("\nDoi xung cua diem {0} la: {1}", diem2d.ConvertToString(), diem2d.DoiXung().ConvertToString());
+            Console.WriteLine("\nDoi xung cua diem {0} la: {1}", diem2d1.ConvertToString(), diem2d1.DoiXung().ConvertToString());
+
+            Console.ReadLine();
+        }
+    }
+}
